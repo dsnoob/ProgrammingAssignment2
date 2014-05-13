15702245d43670b93a4ed962a831f8653c517af9
dsnoob <pv74signups@gmail.com>
2014-05-13 16:18:34 -0700

    added comments to cachesolve method

--- cachematrix.R
+++ cachematrix.R
@@ -4,21 +4,15 @@
 ## @author: dsnoob
 ## @date: May 12, 2014
 
+
 ##
-## Write an R function is able to cache potentially time-consuming computations.
-## If the contents of the vector are not changing then cache them and avoid
-## intensive computations. This assignment gives an oppurtunity to learn the scoping rules
-## of the R language and how they can be manipulated to preserve state inside of an R object.
-##
-
-
-
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  ## initialize the variable
   inverse <- NULL
 
+  ## set the
   set <- function(y) {
     x <<- y
     inverse <<- NULL
@@ -44,7 +38,13 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+##
+## Takes a matrix and gets the inverse of the matrix from
+## cache if it is available. If it is not available then
+## solves to get the inverse and sets in the cache.
+##
+## @param x - a matrix
+##
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
@@ -55,9 +55,9 @@ cacheSolve <- function(x, ...) {
 
     message("calculate inverse...")
 
-    myInv <- x$get()
+    myMatrix <- x$get()
 
-    inv <- solve(myInv, ...)
+    inv <- solve(myMatrix, ...)
 
     x$setInverse(inv)
 
